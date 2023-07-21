45c1ea130297737600bd41f1ebd0c452ae4fe38a
Nicolas Hoizey <nicolas@hoizey.com>
2023-07-21 12:43:10 +0200

    Handle errors

--- netlify/edge-functions/mastodon-avatar.js
+++ netlify/edge-functions/mastodon-avatar.js
@@ -3,11 +3,18 @@ const MASTODON_USERNAME_REGEX =
   /^@?\b([A-Z0-9._%+-]+)@([A-Z0-9.-]+\.[A-Z]{2,})\b$/i;
 
 export default async (request, context) => {
+  console.dir(context);
   const url = new URL(request.url);
   const username = url.searchParams.get("username");
 
+  if (!username) {
+    console.log(`No username defined: ${request.url}`);
+    return await fetch("https://placekitten.com/g/400/400");
+  }
+
   const matches = username.match(MASTODON_USERNAME_REGEX);
   if (matches.length !== 3) {
+    console.log(`No Mastodon username matched: ${JSON.stringify(matches)}`);
     return await fetch("https://placekitten.com/g/400/400");
   }
 
