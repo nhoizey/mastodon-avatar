ca0bfd00f160781a1665204e9e764006cfbd9c2a
Nicolas Hoizey <nicolas@hoizey.com>
2024-06-29 09:57:22 +0200

    Add error to image

--- netlify/edge-functions/mastodon-avatar.js
+++ netlify/edge-functions/mastodon-avatar.js
@@ -52,10 +52,7 @@ export default async (request, context) => {
   } catch (error) {
     console.log(`Couldn't fetch from webfinger URL: ${webfingerUrl}`, error);
     return await fetch(
-      `https://dummyimage.com/400x400/639/fff.png&text=Fetch+error+for+${webfingerUrl.replace(
-        "?",
-        "0x23"
-      )}`
+      `https://dummyimage.com/400x400/639/fff.png&text=Fetch+error:+${error}`
     );
   }
 };
